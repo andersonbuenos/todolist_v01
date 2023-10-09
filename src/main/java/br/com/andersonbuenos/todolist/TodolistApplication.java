d2c31e1f46c9e82708e2d72d78b2bf5201c2aebe
Buenos <adenilson.oliveira@saude.ms.gov.br>
2023-10-09 15:44:04 -0400

    criado componente user

--- src/main/java/br/com/andersonbuenos/todolist/TodolistApplication.java
+++ src/main/java/br/com/andersonbuenos/todolist/TodolistApplication.java
@@ -1,4 +1,4 @@
-package br.com.andersonbunos.todolist;
+package br.com.andersonbuenos.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/br/com/andersonbuenos/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.andersonbuenos.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+}
