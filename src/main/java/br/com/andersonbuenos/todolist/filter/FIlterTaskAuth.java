4ac13dd43d293e42cd066a3825225fb9ca1e925f
andersonbuenos <andersonbuenos@live.com>
2023-10-12 00:13:05 -0400

    criação de regra para autorização ou não de acesso sengundo usuário e senha

---
+++ src/main/java/br/com/andersonbuenos/todolist/filter/FIlterTaskAuth.java
@@ -0,0 +1,66 @@
+package br.com.andersonbuenos.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.aspectj.weaver.BCException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.andersonbuenos.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FIlterTaskAuth extends OncePerRequestFilter{
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+                // SE APRESENTAR ERRO NOS PRÓXIMOS ERROS ABAIXO RETORNAMOS QUE O USUÁRIO NÃO TEM PERMISSÃO
+                // Pegar autenticação (usuário e senha)
+                var authorization = request.getHeader("Authorization");
+
+                var authEncoded = authorization.substring("Basic".length()).trim();
+                
+                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+                var authString = new String(authDecode);
+
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+
+                // Validar usuário
+                var user = this.userRepository.findByUsername(username);
+                if(user == null) {
+                    response.sendError(401);
+                }else {
+                    // Validar senha
+                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                    if(passwordVerify.verified) {
+                        filterChain.doFilter(request, response);
+                    } else {
+                        response.sendError(401);
+                    }
+                    // Segue viagem
+        
+                    
+                }
+
+
+
+                
+    }
+
+
+    
+}
---
+++ src/main/java/br/com/andersonbuenos/todolist/task/ITaskRepository.java
@@ -0,0 +1,10 @@
+package br.com.andersonbuenos.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    
+}
+
